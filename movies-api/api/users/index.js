a0e52a0decb6e1cfe34962e693aa2d4b49b6550c
Emma Grace <20083578@mail.wit.ie>
2020-12-04 16:35:20 +0000

    Week 9 User Favourites

--- movies-api/api/users/index.js
+++ movies-api/api/users/index.js
@@ -3,17 +3,41 @@ import User from './userModel';
 
 const router = express.Router(); // eslint-disable-line
 
-// Get all users
+// old Get all users
 router.get('/', (req, res, next) => { //added next here
     User.find().then(users =>  res.status(200).json(users)).catch(next); //added .catch(next) here
 });
 
+router.get('/:userName/favourites', (req, res, next) => {
+    const user = req.params.userName;
+    User.find( {username: user}).then(
+        user => res.status(201).send(user.favourites)
+    ).catch(next);
+  });
 
 
-// register
+// old post register
 router.post('/', (req, res ,next) => {
     User.create(req.body).then(user => res.status(200).json({success:true,token:"FakeTokenForNow"})).catch(next);
 });
+
+//new post added in week 9 lab
+router.post('/:userName/favourites', (req, res, next) => {
+    const newFavourite = req.body;
+    const query = {username: req.params.userName};
+    if (newFavourite && newFavourite.id) {
+      User.find(query).then(
+        user => {
+          (user.favourites)?user.favourites.push(newFavourite):user.favourites =[newFavourite];
+          User.findOneAndUpdate(query, {favourites:user.favourites}, {
+            new: true
+          }).then(user => res.status(201).send(user));
+        }
+      ).catch(next);
+    } else {
+        res.status(401).send("Unable to find user")
+    }
+  });
 
 // Update a user
 router.put('/:id',  (req, res, next) => { //added next here
