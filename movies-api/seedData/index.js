d0b53b68da75201d6e3485211594b9f4f07c14e3
Emma Grace <20083578@mail.wit.ie>
2020-12-11 14:50:20 +0000

    Topic 10 - Salt and Hash

--- movies-api/seedData/index.js
+++ movies-api/seedData/index.js
@@ -11,12 +11,23 @@ const users = [
   },
 ];
 
-// deletes all user documents in collection and inserts test data
+// deletes all user documents in collection and inserts test data OLD
+// export async function loadUsers() {
+//   console.log('load user Data');
+//     try {
+//       await userModel.deleteMany();
+//       await userModel.collection.insertMany(users);
+//       console.info(`${users.length} users were successfully stored.`);
+//     } catch (err) {
+//       console.error(`failed to Load user Data: ${err}`);
+//     }
+//   }
+
 export async function loadUsers() {
   console.log('load user Data');
     try {
       await userModel.deleteMany();
-      await userModel.collection.insertMany(users);
+      await users.forEach(user => userModel.create(user));
       console.info(`${users.length} users were successfully stored.`);
     } catch (err) {
       console.error(`failed to Load user Data: ${err}`);
